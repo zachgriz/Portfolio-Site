d87f033d9725477fea3e790aa25f49cd73fd6130
zachgriz <zachgrizzly@gmail.com>
2023-10-30 14:26:04 -0700

    fixed styling in experience section. added social icons/links including an emailto link. modified sizing of portrait picture.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Header from './components/header';
 import Navbar from './components/navbar';
 import About from './components/about';
 import Experience from './components/experience';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import { EmailJSResponseStatus } from '@emailjs/browser';
 
 function App() {
   return (
@@ -12,6 +16,17 @@ function App() {
           <Header/>
           <Navbar/>
           <About/>
+          <div className='socials'>
+            <a href="https://www.linkedin.com/in/zach-griswold/">
+              <FontAwesomeIcon icon={faLinkedin} className='icon'/>
+            </a>
+            <a href="https://github.com/zachgriz">
+              <FontAwesomeIcon icon={faGithubSquare} className='icon'/>
+            </a>
+            <a href='mailto:zachgrizzly@gmail.com?subject=Contact via Portfolio'>
+              <FontAwesomeIcon icon={faEnvelopeSquare} className='icon'/>
+            </a>
+          </div>
           <Experience/>
         </div>
       
--- src/components/experience.js
+++ src/components/experience.js
@@ -3,25 +3,36 @@ import '../styles/experience.scss';
 
 export default class Experience extends React.Component{
     render () {
-        return <div className="container" id="experience">
-            <header className="subhead">
+        return <div className="experience" id="experience">
+            <div className="subhead">
                 <span>Work Experience</span>
-            </header>
+            </div>
             <span className="blur"></span>
-            <div className="experience">
-                <p className="title">IDMA3 | Software Solutions Engineer <span>Contract</span></p>
-                <p className="duration">June 2023 - Present</p>
-                <p className="body">Lorem consequat irure aliqua culpa aliquip proident. Magna mollit cupidatat reprehenderit id consectetur excepteur minim non aute cupidatat. Culpa excepteur cupidatat in deserunt aliquip laboris nulla irure nulla Lorem. Adipisicing cillum Lorem laborum sint est cillum est laboris magna. Veniam aute cillum dolor deserunt id amet aliqua sint officia.</p>
-            </div>
-            <div className="experience">
-                <p className="title">Launch Consulting | Software Developer <span>Internship</span></p>
-                <p className="duration">June 2022 - August 2022</p>
-                <p className="body">Lorem consequat irure aliqua culpa aliquip proident. Magna mollit cupidatat reprehenderit id consectetur excepteur minim non aute cupidatat. Culpa excepteur cupidatat in deserunt aliquip laboris nulla irure nulla Lorem. Adipisicing cillum Lorem laborum sint est cillum est laboris magna. Veniam aute cillum dolor deserunt id amet aliqua sint officia.</p>
-            </div>
-            <div className="experience">
-                <p className="title">Washington State University | Data Structures TA <span>Part Time</span></p>
-                <p className="duration">January 2023 - May 2023</p>
-                <p className="body">Lorem consequat irure aliqua culpa aliquip proident. Magna mollit cupidatat reprehenderit id consectetur excepteur minim non aute cupidatat. Culpa excepteur cupidatat in deserunt aliquip laboris nulla irure nulla Lorem. Adipisicing cillum Lorem laborum sint est cillum est laboris magna. Veniam aute cillum dolor deserunt id amet aliqua sint officia.</p>
+            <div className="grid">
+                <div className="item">
+                    <p className="title">IDMA3 | Software Solutions Engineer <span>Contract</span></p>
+                    <p className="duration">June 2023 - Present</p>
+                    <p className="body">Lorem consequat irure aliqua culpa aliquip proident. Magna mollit cupidatat reprehenderit id consectetur excepteur minim non aute cupidatat. Culpa excepteur cupidatat in deserunt aliquip laboris nulla irure nulla Lorem. Adipisicing cillum Lorem laborum sint est cillum est laboris magna. Veniam aute cillum dolor deserunt id amet aliqua sint officia.</p>
+                </div>
+                <div className="logo">
+                    <img></img>
+                </div>
+                <div className="item">
+                    <p className="title">Launch Consulting | Software Developer <span>Internship</span></p>
+                    <p className="duration">June 2022 - August 2022</p>
+                    <p className="body">Lorem consequat irure aliqua culpa aliquip proident. Magna mollit cupidatat reprehenderit id consectetur excepteur minim non aute cupidatat. Culpa excepteur cupidatat in deserunt aliquip laboris nulla irure nulla Lorem. Adipisicing cillum Lorem laborum sint est cillum est laboris magna. Veniam aute cillum dolor deserunt id amet aliqua sint officia.</p>
+                </div>
+                <div className="logo">
+                    <img></img>
+                </div>
+                <div className="item">
+                    <p className="title">Washington State University | Data Structures TA <span>Part Time</span></p>
+                    <p className="duration">January 2023 - May 2023</p>
+                    <p className="body">Lorem consequat irure aliqua culpa aliquip proident. Magna mollit cupidatat reprehenderit id consectetur excepteur minim non aute cupidatat. Culpa excepteur cupidatat in deserunt aliquip laboris nulla irure nulla Lorem. Adipisicing cillum Lorem laborum sint est cillum est laboris magna. Veniam aute cillum dolor deserunt id amet aliqua sint officia.</p>
+                </div>
+                <div className="logo">
+                    <img></img>
+                </div>
             </div>
         </div>
     }
